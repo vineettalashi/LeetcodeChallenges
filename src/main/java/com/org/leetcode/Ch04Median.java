1a768c9ac6ca968b7fded27d0d6ac81ce8c03e91
Vineet Talashi <vineettalashi@gmail.com>
2019-09-16 02:22:08 +0530

    Update Ch04Median.java

--- src/main/java/com/org/leetcode/Ch04Median.java
+++ src/main/java/com/org/leetcode/Ch04Median.java
@@ -46,25 +46,18 @@ public class Ch04Median {
 		return median;
     }
     
-    static int[] insertionSort(int arr[], int n)  
-    {  
+    static int[] insertionSort(int arr[], int n) {  
         int i, key, j;  
         for (i = 1; i < n; i++) 
         {  
             key = arr[i];  
-            j = i - 1;  
-      
-            /* Move elements of arr[0..i-1], that are  
-            greater than key, to one position ahead  
-            of their current position */
-            while (j >= 0 && arr[j] > key) 
-            {  
+            j = i - 1;
+            while (j >= 0 && arr[j] > key) {  
                 arr[j + 1] = arr[j];  
                 j = j - 1;  
             }  
             arr[j + 1] = key;  
-        } 
-        
+        }        
         return arr;
     }  
 
